0e28592ee9c2f9a8f8d373b37d5aad6481c299ae
Mikhail Stolpovskiy <m-stol@yandex.ru>
2020-02-03 17:30:12 +0100

    DmpTrackSelector class added

--- include/track_selection.hpp
+++ include/track_selection.hpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
+
 #include "TClonesArray.h"
 
 #include "DmpEvtHeader.h"
@@ -11,12 +13,17 @@
 #include "DmpStkSiCluster.h"
 #include "DmpStkTrackHelper.h"
 #include "DmpSvcPsdEposCor.h"
+#include "DmpStkTrack.h"
+#include "DmpEvent.h"
+#include "DmpEvtPsdRec.h"
 
 #define NLADDERS     192
 #define NCHANNELS    384
 #define NLAYERS      6
-#define VA           6
-#define ETA          2
+#define NPSDLAYERS   2
+#define NPSDBARS     41
+// #define VA           6
+// #define ETA          2
 
 // Define track cuts
 #define MINTRACKHITS 6 // 5
@@ -27,3 +34,46 @@
 bool** read_bad_channels_file(const char* filename);
 bool is_cluster_bad_channel(DmpStkSiCluster * cluster, bool** badchannels);
 int reaction_in_psd(DmpStkTrackHelper * stk_helper, DmpEvtPsdRec *psdRec, bool mc=false);
+
+class DmpTrackSelector {
+public:
+    DmpTrackSelector();
+    DmpTrackSelector(const char * file);
+    ~DmpTrackSelector();
+
+    enum Select{stk_bad_channel,
+                psd_match
+                }
+
+    void setSelectTypes(vector<Select> type) {mSelectTypes = types;}
+    vector<Select> selectTypes() const {return mSelectTypes;}
+
+    /**
+     * Add selection criterium
+     */
+    void addSelect(Select type);
+
+    /**
+     * Returns true, if the track is selected.
+     * event is the DmpEvent object, that contains the track
+     */    
+    bool selected(DmpStkTrack * track, DmpEvent * event) const;
+
+    /**
+     * Returns true, if the track passes the type selection
+     * event is the DmpEvent object, that contains the track
+     */    
+    bool pass(DmpStkTrack * track, DmpEvent * event, Select type) const;
+
+private:
+    vector<Select> mSelectTypes;
+
+    // Bad channel check
+    vector<vector<bool>> mBadChannelList;
+    char * mBadChannelsFile;
+    void readBadChannelsFile();
+    bool hasBadChannel(DmpStkTrack * track) const;
+
+    // PSD match
+    bool psdMatch(DmpStkTrack * tracl, DmpEvent * event) const;
+}
--- source/track_selection.cpp
+++ source/track_selection.cpp
@@ -1,94 +1,139 @@
 #include "track_selection.hpp"
 
 
-bool** read_bad_channels_file(const char* filename){
-	bool** badchannels = new bool*[NLADDERS];
-	for(int i=0; i<NLADDERS; i++){
-		badchannels[i] = new bool[NCHANNELS];
-		for(int j=0; j<NCHANNELS; j++){
-			badchannels[i][j] = false;
-		}
-	}
+DmpTrackSelector::DmpTrackSelector() 
+{
+    mSelectTypes.resize(0);
 
-	ifstream infile(filename);	
-	if (!infile)
-	{
-		std::cout<<"Can't open Bad Channels file: "<<filename<<" ==> throwing exception!"<<std::endl;
-		throw;
-	}
-	std::string line;
-	while (getline(infile, line)) {
-		if (line.c_str() == std::string("")) break;
-		std::istringstream lineStream(line.c_str());
-		std::string number;
-		int i = 0;
-		int ildr   = -1;
-		int bdchnl = -1;
-		while (getline(lineStream, number, ',')) {
-			switch (i++) {
-			case 0:
-				ildr = atoi(number.c_str());
-				break;
-			case 1:
-				bdchnl = atoi(number.c_str());
-				break;			
-			}
-		}
-		if(ildr>=0 && bdchnl>=0 && ildr<NLADDERS && bdchnl<NCHANNELS){
-			badchannels[ildr][bdchnl] = true;
-		}
-	}
-	std::cout<<"Done reading bad channels"<<std::endl;
-	return badchannels;
-} // end read_bad_channels_file
-
-
-bool is_cluster_bad_channel(DmpStkSiCluster * cluster, bool** badchannels) {
-    int ladder = cluster->getLadderHardware(); //ladder num
-    int minc = cluster->getIndex1();//address of the first strip in the cluster (0-363)
-    int maxc = cluster->getIndex1() + cluster->getNstrip() -1;//last strip of the cluster
-
-	for(int i = minc; i <= maxc; i++){
-		if(badchannels[ladder][i]) return true;
-	}
-	return false;
-} // end is_cluster_bad_channel
-
-
-int reaction_in_psd(DmpStkTrackHelper * stk_helper, DmpEvtPsdRec *psdRec, bool mc) {
-	// returns 0 if it seems like the first inelastic interaction
-	// took place in the first PSD layer or above it
-	// returns 1 if the first II took place in the second layer of the PSD
-	// If the first interaction was in STK or below, return 2
-	// If there is no track, return 3
-    int ntracks = stk_helper->GetSize();
-
-    if (ntracks == 0) return 3;
-    if (ntracks == 1) return 2;
-
-    int count_fired_bars[2] = {0, 0};
-
-    for(int tck = 0; tck < stk_helper->GetSize(); tck++) {
-        DmpStkTrack* stktrack = stk_helper->GetTrack(tck);
-
-	    TVector3 impact = stktrack->getImpactPoint();
-	    TVector3 direction = stktrack->getDirection();
-
-	    for (int ilayer = 0; ilayer < 2; ilayer++) {
-	        // Loop over psd bars
-	        for (int ibar = 0; ibar < 41; ibar++) {
-	        	double e = psdRec->GetEdep(ilayer, ibar);
-	        	if (e == 0) continue;
-	            double * len = new double[2];
-	            if(!mc && !gPsdECor->GetPathLengthPosition(ilayer, ibar, direction, impact, len))
-	                continue;
-	            if(mc && !gPsdECor->GetPathLPMC(ilayer, ibar, direction, impact, len))
-	                continue;
-                count_fired_bars[ilayer] ++;
-			}
-		}
-		if (count_fired_bars[0] > 3) return 0;
-		if (count_fired_bars[1] > 3) return 1;
+    mBadChannelList.resize(NLADDERS);
+    for (vector<vector<bool>>::iterator it = mBadChannelList.begin();
+         it != mBadChannelList.end();
+         ++it) {
+        it->resize(NCHANNELS);
     }
-    return 2;
+}
+
+DmpTrackSelector::DmpTrackSelector(const char * file) :
+    DmpTrackSelector()
+{
+    mBadChannelsFile = file;
+} 
+
+void DmpTrackSelector::addSelect(Select type){
+    switch (type) {
+        case stk_bad_channel :
+            readBadChannelsFile(mBadChannelsFile);
+            break;
+        case psd_match :
+        default :
+            break;
+    }
+    mSelectTypes.push_bash type;
+}
+
+bool DmpTrackSelector::selected(DmpStkTrack* track) const {
+    for (vector<Select>::iterator it = mSelectTypes.begin();
+         it != mSelectTypes.end(); 
+         ++it) {
+        if(!pass(track, *it)) return false;
+    }
+    return true
+}
+
+bool DmpTrackSelector::pass(DmpStkTrack * track, DmpEvent * event, Select type) const {
+    switch (type) {
+        case stk_bad_channel :
+            return hasBadChannel(track);
+            break;
+        case psdMatch :
+            return psdMatch(track);
+        default :
+            return true;
+    }
+}
+
+void DmpTrackSelector::readBadChannelsFile() {
+    ifstream infile(mBadChannelsFile);  
+    if (!infile)
+    {
+        std::cout << "Can't open Bad Channels file: " << mBadChannelsFile;
+        std::cout << " ==> throwing exception!" << std::endl;
+        throw;
+    }
+
+    std::string line;
+    while (getline(infile, line)) {
+        if (line.c_str() == std::string("")) break;
+        std::istringstream lineStream(line.c_str());
+        std::string number;
+        int i = 0;
+        int ildr   = -1;
+        int bdchnl = -1;
+        while (getline(lineStream, number, ',')) {
+            switch (i++) {
+            case 0:
+                ildr = atoi(number.c_str());
+                break;
+            case 1:
+                bdchnl = atoi(number.c_str());
+                break;          
+            }
+        }
+        if(ildr>=0 && bdchnl>=0 && ildr<NLADDERS && bdchnl<NCHANNELS){
+            mBadChannelList[ildr][bdchnl] = true;
+        }
+    }
+}
+
+bool DmpTrackSelector::hasBadChannel(DmpStkTrack * track) const{
+    // Loop over clusters
+    for (int ipoint=0; ipoint<stktrack->GetNPoints(); ipoint++) {
+        for (int ixy=0; ixy<2; ixy++) {
+            DmpStkSiCluster* cluster;
+            if(ixy == 0)
+                cluster = stktrack -> GetClusterX(ipoint, stkclusters);
+            else
+                cluster = stktrack -> GetClusterY(ipoint, stkclusters);
+            if(!cluster) continue;
+
+            //ladder num
+            int ladder = cluster->getLadderHardware();
+
+            //address of the first strip in the cluster (0-363)
+            int minc = cluster->getIndex1();
+
+            //last strip of the cluster
+            int maxc = cluster->getIndex1() + cluster->getNstrip() -1;
+
+            for(int i = minc; i <= maxc; i++){
+                if(mBadChannelList[ladder][i]) return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool DmpTrackSelector::psdMatch(DmpStkTrack * tracl, DmpEvent * event) const {
+    DmpEvtPsdRec *psdRec = pev->pEvtPsdRec();
+
+    TVector3 impact = stktrack->getImpactPoint();
+    TVector3 direction = stktrack->getDirection();
+
+    bool mc = (pev->pEvtSimuHeader() != nullptr);
+
+    // Loop over psd layers
+    for (int ilayer = 0; ilayer < NPSDLAYERS; ilayer++) {
+        // Loop over psd bars
+        for (int ibar = 0; ibar < NPSDBARS; ibar++) {
+            double e = psdRec->GetEdep(ilayer, ibar);
+            if (e == 0) continue;
+            double * len = new double[2];
+            if(!mc && !gPsdECor->GetPathLengthPosition(ilayer, ibar, direction, impact, len))
+                continue;
+            if(mc && !gPsdECor->GetPathLPMC(ilayer, ibar, direction, impact, len))
+                continue;
+            return true;
+        }
+    }
+    return false;
 }
