b8dc16e9203c10d9abb0f1da7b4440bb4049de59
Mikhail Stolpovskiy <m-stol@yandex.ru>
2020-02-04 15:03:39 +0100

    Add progress control

--- source/analysis.cpp
+++ source/analysis.cpp
@@ -61,6 +61,12 @@ void myDampeLib::DmpAnalysis::add2TChain(string filename, bool verbose/*=true*/)
 void myDampeLib::DmpAnalysis::run(int n/*=-1*/)
 { 
     int nnn=(n < 0)? mNEvents : n;
-    for(int i=0; i<nnn; i++) 
+    for(int i=0; i<nnn; i++) {
+        if ((i % (nnn / 10 + 1)) == int(nnn / 10)) {
+            int percentage = 10. * int((i + 1) / int(nnn / 10));
+            cout << "Processing percentage: " << percentage << "% \n";
+        }
+
         analyseOneEvent();
+    }
 }
